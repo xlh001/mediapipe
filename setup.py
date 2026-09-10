fa9ba118346a0f7e8f94e08080e15383a9437564
Sebastian Schmidt <mrschmidt@google.com>
2026-09-10 08:15:01 -0700

    Add Python 3.13 and 3.14 support to the MediaPipe Bazel build.
    PiperOrigin-RevId: 979194127

--- setup.py
+++ setup.py
@@ -414,6 +414,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
